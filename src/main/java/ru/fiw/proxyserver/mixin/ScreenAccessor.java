553b9d2e8e798ee948505d8dc893813865df0eb7
StarsShine11904 <89504980+StarsShine11904@users.noreply.github.com>
2026-09-06 21:46:36 +0800

    Add invoker for addRenderableWidget method

--- src/main/java/ru/fiw/proxyserver/mixin/ScreenAccessor.java
+++ src/main/java/ru/fiw/proxyserver/mixin/ScreenAccessor.java
@@ -9,6 +9,7 @@ import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(Screen.class)
 public interface ScreenAccessor {
+
     @Invoker("addRenderableWidget")
     <T extends GuiEventListener & Renderable & NarratableEntry> T invokeAddRenderableWidget(T widget);
 }
