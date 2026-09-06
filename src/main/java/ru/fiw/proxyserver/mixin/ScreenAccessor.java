100795f89ad2fc1b8829ed5aaca0f2ccd0b26dc8
StarsShine11904 <89504980+StarsShine11904@users.noreply.github.com>
2026-09-06 21:45:57 +0800

    Refactor ScreenAccessor to use Invoker for widgets

--- src/main/java/ru/fiw/proxyserver/mixin/ScreenAccessor.java
+++ src/main/java/ru/fiw/proxyserver/mixin/ScreenAccessor.java
@@ -1,23 +1,14 @@
 package ru.fiw.proxyserver.mixin;
 
+import net.minecraft.client.gui.components.Renderable;
 import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.narration.NarratableEntry;
-import net.minecraft.client.gui.components.Renderable;
 import net.minecraft.client.gui.screens.Screen;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
-
-import java.util.List;
+import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(Screen.class)
 public interface ScreenAccessor {
-
-    @Accessor("renderables")
-    List<Renderable> getRenderables();
-
-    @Accessor("children")
-    List<GuiEventListener> getChildren();
-
-    @Accessor("narratables")
-    List<NarratableEntry> getNarratables();
+    @Invoker("addRenderableWidget")
+    <T extends GuiEventListener & Renderable & NarratableEntry> T invokeAddRenderableWidget(T widget);
 }
